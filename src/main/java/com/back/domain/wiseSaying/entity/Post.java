93b0a2d009a03c365151e3164b764621facff777
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-13 14:49:57 +0900

    ì기존 db 대신 다른거 만들기

--- src/main/java/com/back/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/back/domain/wiseSaying/entity/Post.java
@@ -3,14 +3,14 @@ package com.back.domain.wiseSaying.entity;
 import jakarta.persistence.*;
 
 @Entity
-public class WiseSaying {
+public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(length = 100, nullable = false)
-    private String content;
+    private String title;
 
-    @Column(length = 50, nullable = false)
-    private String author;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
 }
