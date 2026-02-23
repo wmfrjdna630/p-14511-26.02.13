0a9c1d72078f51c8d7ea6fd7c9701d66c9ad1818
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-23 11:44:08 +0900

    PostRepository 대신 PostService ì사용

--- src/main/java/com/back/domain/post/service/PostService.java
+++ src/main/java/com/back/domain/post/service/PostService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PostService {
@@ -19,8 +20,8 @@ public class PostService {
         return postRepository.save(post);
     }
 
-    public Post findById(int id) {
-        return postRepository.findById(id).orElse(null);
+    public Optional<Post> findById(int id) {
+        return postRepository.findById(id);
     }
 
     public List<Post> findAll() {
--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -1,7 +1,6 @@
 package com.back.global;
 
-import com.back.domain.post.entity.Post;
-import com.back.domain.post.repository.PostRepository;
+import com.back.domain.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class BaseInitData {
 
     @Autowired
-    private PostRepository postRepository;
+    private PostService postService;
 
     @Bean
     ApplicationRunner initDataRunner() {
@@ -25,19 +24,16 @@ public class BaseInitData {
 
     void work1() {
 
-        if(postRepository.count() > 0) {
+        if(postService.count() > 0) {
             return;
         }
 
-        Post post1 = new Post("제목1", "내용1");
-        postRepository.save(post1);
-
-        Post post2 = new Post("제목2", "내용2");
-        postRepository.save(post2);
+        postService.write("제목1", "내용1");
+        postService.write("제목2", "내용2");
     }
 
     void work2() {
-        postRepository.findById(1);
+        postService.findById(1);
         // select * from post where id = 1;
     }
 
