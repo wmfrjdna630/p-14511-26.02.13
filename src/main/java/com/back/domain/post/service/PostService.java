7bfe5b72c61b90f0fe4add0b2d50c9c72b5d380e
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-23 12:49:03 +0900

    PostTransaction

--- src/main/java/com/back/domain/post/service/PostService.java
+++ src/main/java/com/back/domain/post/service/PostService.java
@@ -2,17 +2,19 @@ package com.back.domain.post.service;
 
 import com.back.domain.post.entity.Post;
 import com.back.domain.post.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Component
+@Service
 public class PostService {
 
-    @Autowired
     private PostRepository postRepository;
+
+    public PostService(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
 
     public Post write(String title, String content) {
 
--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -1,35 +1,48 @@
 package com.back.global;
 
 import com.back.domain.post.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class BaseInitData {
 
-    @Autowired
-    private PostService postService;
+    private final PostService postService;
 
     @Bean
     ApplicationRunner initDataRunner() {
         return args -> {
 
             work1();
-
             work2();
         };
     }
 
+    @Transactional
     void work1() {
 
-        if(postService.count() > 0) {
-            return;
-        }
+        try {
 
-        postService.write("제목1", "내용1");
-        postService.write("제목2", "내용2");
+            if (postService.count() > 0) {
+                return;
+            }
+
+            // 어떤 기능을 테스트 데이터가 2개인 것을 가정하고 개발
+            postService.write("제목1", "내용1");
+
+            if (true) {
+                throw new RuntimeException("테스트 예외");
+            }
+
+            postService.write("제목2", "내용2");
+
+        } catch (Exception e) {
+            System.out.println("예외 발생 : " + e.getMessage());
+        }
     }
 
     void work2() {
