81ea791d1ebc62b3714eb631709a04cda5594b49
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-23 14:39:05 +0900

    Transactional(readOnly = true)

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -1,15 +1,22 @@
 package com.back.global;
 
 import com.back.domain.post.service.PostService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BaseInitData {
+
+    @Autowired
+    @Lazy
+    private BaseInitData self;
 
     private final PostService postService;
 
@@ -17,37 +24,32 @@ public class BaseInitData {
     ApplicationRunner initDataRunner() {
         return args -> {
 
-            work1();
-            work2();
+            self.work1();
+            self.work2();
         };
     }
 
     @Transactional
     void work1() {
 
-        try {
-
-            if (postService.count() > 0) {
-                return;
-            }
-
-            // 어떤 기능을 테스트 데이터가 2개인 것을 가정하고 개발
-            postService.write("제목1", "내용1");
-
-            if (true) {
-                throw new RuntimeException("테스트 예외");
-            }
-
-            postService.write("제목2", "내용2");
-
-        } catch (Exception e) {
-            System.out.println("예외 발생 : " + e.getMessage());
+        if (postService.count() > 0) {
+            return;
         }
+
+        // 어떤 기능을 테스트 데이터가 2개인 것을 가정하고 개발
+        postService.write("제목1", "내용1");
+
+//        if (true) {
+//            throw new RuntimeException("테스트 예외");
+//        }
+
+        postService.write("제목2", "내용2");
     }
 
     void work2() {
         postService.findById(1);
         // select * from post where id = 1;
     }
+
 
 }
