2a5bf8a1d166b81edc97d9dc54089a47044f0b65
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-23 10:47:24 +0900

    findById

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -25,6 +25,8 @@ public class BaseInitData {
 
             Post post1 = new Post("제목1", "내용1");
             postRepository.save(post1);
+
+            postRepository.findById(1);
         };
     }
 
